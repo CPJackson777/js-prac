b79b54b4239d8ad6be92a685aa98d7dc13b37b32
Charles Jackson <cpjackson572@gmail.com>
2020-10-24 23:39:38 -0500

    added html and js files

---
+++ src/main.js
@@ -0,0 +1,37 @@
+for ( let i = 1; i <= 100; i++) {
+    if ((i % 5 === 0) && (i % 7 === 0)) {
+        console.log("FizzBuzz")
+    } else if (i % 5 === 0) {
+        console.log("Fizz")
+    } else if (i % 7 === 0) {
+        console.log("Buzz")
+    } else {
+        console.log(i)
+    }
+}
+
+for (let i = 1; i <= 100; i++) {
+    if ((i % 5 === 0) && (i % 7 === 0)) {
+        console.log("ChickenMonkey")
+    } else if (i % 5 === 0) { 
+        console.log("Chicken")
+    } else if (i % 7 === 0) { 
+        console.log("Monkey")
+    } else {
+        console.log(i)
+    }
+}
+
+//reverse a string
+function reverse(string) {
+    console.log(string.split('').reverse().join(''))
+}
+
+reverse("Charles Jackson is cool")
+
+let str = "I'm so cold."
+console.log(str.split('').reverse().join(''))
+
+
+
+const sheeps = ['🐑', '🐑', '🐑'];
