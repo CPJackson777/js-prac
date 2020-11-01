09fcb8def64877d48e2f26e9260ef7dab6f2ad33
Charles Jackson <cpjackson572@gmail.com>
2020-10-31 23:15:16 -0500

    added comment

--- src/main.js
+++ src/main.js
@@ -58,3 +58,5 @@ Object.keys(lunch).forEach(function (item) {
     console.log(item); // key
     console.log(lunch[item]); // value
 });
+
+// returns "sandwich", "ham", "snack", "chips", "drink", "soda", "desert", "cookie", "guests", 3, "alcohol", false
